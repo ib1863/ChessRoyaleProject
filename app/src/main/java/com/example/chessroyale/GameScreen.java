a685c80e9a97f97b27cfc200ff24fd409c656230
Idan Bunkin <idan.bunkin@gmail.com>
2024-01-04 12:20:32 +0200

    finished login and register.

--- app/src/main/java/com/example/chessroyale/GameScreen.java
+++ app/src/main/java/com/example/chessroyale/GameScreen.java
@@ -1,14 +1,53 @@
 package com.example.chessroyale;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class GameScreen extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    FirebaseUser user;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        user = auth.getCurrentUser();
+        if(user == null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("stayConnect", false);
+                editor.commit();
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chessroyale/MainActivity.java
+++ app/src/main/java/com/example/chessroyale/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.chessroyale;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,6 +28,28 @@ public class MainActivity extends AppCompatActivity {
     Button buttonReg, buttonSignIn;
     FirebaseAuth mAuth;
 
+    ProgressBar progressBar;
+
+    CheckBox stayConnected;
+
+    SharedPreferences settings;
+
+    public static FirebaseAuth refAuth =FirebaseAuth.getInstance();
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        settings = getSharedPreferences("PREFS_NAME", MODE_PRIVATE);
+        Boolean isChecked = settings.getBoolean("stayConnect", false);
+        if(isChecked && refAuth.getCurrentUser() != null)
+        {
+            Intent intent = new Intent(getApplicationContext(), GameScreen.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +59,18 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         buttonReg = findViewById(R.id.register);
         buttonSignIn = findViewById(R.id.signIn);
+        progressBar = findViewById(R.id.progressBar);
+        stayConnected = findViewById(R.id.checkBox);
+
+
+
+
+
 
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email, password;
                 email = String.valueOf(editTextEmail.getText());
                 password = String.valueOf(editTextPassword.getText());
@@ -54,10 +89,21 @@ public class MainActivity extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task){
+                                progressBar.setVisibility(View.GONE);
                                 if(task.isSuccessful()) {
+                                    if(stayConnected.isChecked()){
+                                        SharedPreferences.Editor editor = settings.edit();
+                                        editor.putBoolean("stayConnect", stayConnected.isChecked());
+                                        editor.commit();
+                                    }
                                     Toast.makeText(MainActivity.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), GameScreen.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
+                                    editTextEmail.setText("");
+                                    editTextPassword.setText("");
                                     Toast.makeText(MainActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                 }
@@ -65,6 +111,55 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         });
+
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(MainActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    if(stayConnected.isChecked()){
+                                        SharedPreferences.Editor editor = settings.edit();
+                                        editor.putBoolean("stayConnect", stayConnected.isChecked());
+                                        editor.commit();
+                                    }
+                                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), GameScreen.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    editTextEmail.setText("");
+                                    editTextPassword.setText("");
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
     }
 
 }
